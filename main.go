596f68c64833c912d4ca8d2c3117b969cfbbd709
Ivan Boychuk <boychuk@divo-games.com>
2017-12-01 15:10:27 +0300

    Refactor project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "tournament"
+)
+
+func main(){
+    // Add routes
+    router := tournament.NewRouter()
+    tournament.DataInitialization()
+    // Initialize players and tournament
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
